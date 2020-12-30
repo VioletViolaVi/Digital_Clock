af5f3cf295751d113e1ff87543c70977c233fc26
VioletViolaVi <vivian.v18723@gmail.com>
2020-12-30 15:55:44 +0000

    Add function to get today's time.

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,18 +1,29 @@
 document.addEventListener("DOMContentLoaded", function() { 
     
     // Write a JavaScript program to display the current day and time in the following format
-   
+    
+    let todaysFullDate = new Date();   
     /* gets textual day of the week */
     let daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     let emptyDay = document.getElementById("day");
+    let emptyTime = document.getElementById("time"); 
 
-    /* gets NUMBER for indexing weekday */
-    function getDay() {
-        let dayNumForToday = new Date().getDay();
+    /* gets NUMBER for indexing weekdays */
+    function getDayNum() {
+        let dayNumForToday = todaysFullDate.getDay();
         let today = daysOfTheWeek[dayNumForToday];
         emptyDay.append(today);
     }
-    getDay();
+    getDayNum();
+    
+    /* gets NUMBERS for current hour(s) and minute(s) */
+    function getTimeNums() {
+        let hours = todaysFullDate.getHours();
+        let minutes = todaysFullDate.getMinutes();
+        let todaysTime = hours + ":" + minutes;        
+        emptyTime.append(todaysTime);
+    }
+    getTimeNums();
 
 
 
