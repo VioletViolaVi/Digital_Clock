d434a8fd40b26cb72d9bc35280a4a3d1ded20a99
VioletViolaVi <vivian.v18723@gmail.com>
2020-12-30 16:06:01 +0000

    Add readme details and refactored code.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # JavaScript Practice
+This consists of code practicing the use of JavaScript by solving the exercises presented on the
+[w3resource](https://www.w3resource.com/javascript-exercises/javascript-basic-exercises.php) website.
--- assets/scripts.js
+++ assets/scripts.js
@@ -1,54 +1,32 @@
-document.addEventListener("DOMContentLoaded", function() { 
-    
-    // Write a JavaScript program to display the current day and time in the following format
-    
-    let todaysFullDate = new Date();   
-    /* gets textual day of the week */
-    let daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    let emptyDay = document.getElementById("day");
-    let emptyTime = document.getElementById("time"); 
+document.addEventListener("DOMContentLoaded", function () {
+  // Write a JavaScript program to display the current day and time in the following format
+  let todaysFullDate = new Date(); // gets today's day, date & time
+  let daysOfTheWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ]; // gets textual day of the week
+  let emptyDay = document.getElementById("day"); // gets span w/ id="day"
+  let emptyTime = document.getElementById("time"); // gets span w/ id="time"
 
-    /* gets NUMBER for indexing weekdays */
-    function getDayNum() {
-        let dayNumForToday = todaysFullDate.getDay();
-        let today = daysOfTheWeek[dayNumForToday];
-        emptyDay.append(today);
-    }
-    getDayNum();
-    
-    /* gets NUMBERS for current hour(s) and minute(s) */
-    function getTimeNums() {
-        let hours = todaysFullDate.getHours();
-        let minutes = todaysFullDate.getMinutes();
-        let todaysTime = hours + ":" + minutes;        
-        emptyTime.append(todaysTime);
-    }
-    getTimeNums();
+  /* gets NUMBER for indexing weekdays */
+  function getDayIndexNum() {
+    let dayNumForToday = todaysFullDate.getDay();
+    let today = daysOfTheWeek[dayNumForToday];
+    emptyDay.append(today);
+  }
+  getDayIndexNum();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  });
+  /* gets NUMBERS for current hour(s) and minute(s) */
+  function getTimeNums() {
+    let hours = todaysFullDate.getHours();
+    let minutes = todaysFullDate.getMinutes();
+    let todaysTime = hours + ":" + minutes;
+    emptyTime.append(todaysTime);
+  }
+  getTimeNums();
+});
