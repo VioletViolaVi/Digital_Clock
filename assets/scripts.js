531c2c00278cb366c0a9fc7a7f4482c59b7844e9
VioletViolaVi <vivian.v18723@gmail.com>
2020-12-30 16:37:23 +0000

    Add functions for adding 0 for 1-9 numbers.

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,6 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Write a JavaScript program to display the current day and time in the following format
+  // Write a JavaScript program to display the current day and time.
+
   let todaysFullDate = new Date(); // gets today's day, date & time
+  let hours = todaysFullDate.getHours(); // gets num for hours
+  let minutes = todaysFullDate.getMinutes(); // gets num for minutes
+  // gets textual day of the week
   let daysOfTheWeek = [
     "Sunday",
     "Monday",
@@ -9,11 +13,11 @@ document.addEventListener("DOMContentLoaded", function () {
     "Thursday",
     "Friday",
     "Saturday",
-  ]; // gets textual day of the week
+  ];
   let emptyDay = document.getElementById("day"); // gets span w/ id="day"
   let emptyTime = document.getElementById("time"); // gets span w/ id="time"
 
-  /* gets NUMBER for indexing weekdays */
+  /* gets NUMBER for indexing weekday array */
   function getDayIndexNum() {
     let dayNumForToday = todaysFullDate.getDay();
     let today = daysOfTheWeek[dayNumForToday];
@@ -21,12 +25,26 @@ document.addEventListener("DOMContentLoaded", function () {
   }
   getDayIndexNum();
 
-  /* gets NUMBERS for current hour(s) and minute(s) */
+  /* assembles hour and minute numbers together */
   function getTimeNums() {
-    let hours = todaysFullDate.getHours();
-    let minutes = todaysFullDate.getMinutes();
+    addZeroToSingleHours();
+    addZeroToSingleMinutes();
     let todaysTime = hours + ":" + minutes;
     emptyTime.append(todaysTime);
   }
   getTimeNums();
+
+  /* adds 0 in front for hour nums 1-9 */
+  function addZeroToSingleHours() {
+    if (hours >= 0 && hours < 10) {
+      hours = "0" + hours;
+    }
+  }
+
+  /* adds 0 in front for minute nums 1-9 */
+  function addZeroToSingleMinutes() {
+    if (minutes >= 0 && minutes < 10) {
+      minutes = "0" + minutes;
+    }
+  }
 });
