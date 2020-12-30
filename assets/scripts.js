f0bc2d5586e0fc5923e3b117d91a0aae0940c39a
VioletViolaVi <vivian.v18723@gmail.com>
2020-12-30 15:28:09 +0000

    Create function to get current day of week.

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,6 +1,22 @@
 document.addEventListener("DOMContentLoaded", function() { 
-    console.log("Hello" + " " + "World" + "!");
     
+    // Write a JavaScript program to display the current day and time in the following format
+   
+    /* gets textual day of the week */
+    let daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let emptyDay = document.getElementById("day");
+
+    /* gets NUMBER for indexing weekday */
+    function getDay() {
+        let dayNumForToday = new Date().getDay();
+        let today = daysOfTheWeek[dayNumForToday];
+        emptyDay.append(today);
+    }
+    getDay();
+
+
+
+
 
 
 
