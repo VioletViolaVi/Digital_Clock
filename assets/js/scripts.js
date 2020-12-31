f2f94b3173a6fc8c54e85b9db23c0f0ae8bb9850
VioletViolaVi <vivian.v18723@gmail.com>
2020-12-31 13:32:31 +0000

    Add seconds to clock.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,13 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
-  let todaysFullDate = new Date(); // gets today's day, date & time
-
-  let hours = todaysFullDate.getHours(); // gets num for hours
-  let minutes = todaysFullDate.getMinutes(); // gets num for minutes
-
+  // from html file
   let emptyTime = document.getElementById("time"); // gets id="time"
+  let emptySeconds = document.getElementById("seconds"); // gets id="seconds"
   let emptyMonth = document.getElementById("month"); // gets id="month"
   let emptyMonthDate = document.getElementById("monthDate"); // gets id="monthDate"
   let emptyDay = document.getElementById("weekday"); // gets id="weekday"
+
+  // from js
+  let todaysFullDate = new Date(); // gets today's day, date & time
+  let hours = todaysFullDate.getHours(); // gets num for hours
+  let minutes = todaysFullDate.getMinutes(); // gets num for minutes
+  let seconds = todaysFullDate.getSeconds(); // gets num for seconds
 
   // textual months
   let monthsOfTheYear = [
@@ -67,6 +70,13 @@ document.addEventListener("DOMContentLoaded", function () {
   }
   getTimeNums();
 
+  /* gets sec numbers */
+  function getSeconds() {
+    addZeroToSingleSeconds();
+    emptySeconds.append(seconds);
+  }
+  getSeconds();
+
   /* adds 0 in front for hour nums 1-9 */
   function addZeroToSingleHours() {
     if (hours >= 0 && hours < 10) {
@@ -78,6 +88,13 @@ document.addEventListener("DOMContentLoaded", function () {
   function addZeroToSingleMinutes() {
     if (minutes >= 0 && minutes < 10) {
       minutes = "0" + minutes;
+    }
+  }
+
+  /* adds 0 in front for sec nums 1-9 */
+  function addZeroToSingleSeconds() {
+    if (seconds >= 0 && seconds < 10) {
+      seconds = "0" + seconds;
     }
   }
 });
